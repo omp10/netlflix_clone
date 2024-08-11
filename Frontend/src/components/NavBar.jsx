c6b338c0df4c95ac42e430ceda243500b4073bd8
omp10 <omparteki2002@gmail.com>
2024-08-11 19:11:38 +0530

    bugs fixed for mobile

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -7,10 +7,9 @@ import { useContentStore } from "../store/content";
 const Navbar = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 	const { user, logout } = useAuthStore();
+	const { setContentType } = useContentStore();
 
 	const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
-
-	const { setContentType } = useContentStore();
 
 	return (
 		<header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
@@ -37,7 +36,7 @@ const Navbar = () => {
 				<Link to={"/search"}>
 					<Search className='size-6 cursor-pointer' />
 				</Link>
-				<img src={user.image} alt='Avatar' className='h-8 rounded cursor-pointer' />
+				<img src={user.image || '/default-avatar.png'} alt='Avatar' className='h-8 rounded cursor-pointer' />
 				<LogOut className='size-6 cursor-pointer' onClick={logout} />
 				<div className='sm:hidden'>
 					<Menu className='size-6 cursor-pointer' onClick={toggleMobileMenu} />
@@ -47,10 +46,10 @@ const Navbar = () => {
 			{/* mobile navbar items */}
 			{isMobileMenuOpen && (
 				<div className='w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800'>
-					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+					<Link to={"/"} className='block hover:underline p-2' onClick={() => { setContentType("movie"); toggleMobileMenu(); }}>
 						Movies
 					</Link>
-					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+					<Link to={"/"} className='block hover:underline p-2' onClick={() => { setContentType("tv"); toggleMobileMenu(); }}>
 						Tv Shows
 					</Link>
 					<Link to={"/history"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
@@ -61,4 +60,5 @@ const Navbar = () => {
 		</header>
 	);
 };
+
 export default Navbar;
